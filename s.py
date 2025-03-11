cd001861027fc43f65650ae0242c4e4fbce69cc1
Jane2 <jsiiewdwty@gmail.com>
2025-03-12 01:04:14 +0900

    main3에 올리기

--- s.py
+++ s.py
@@ -2,10 +2,7 @@ class BMRCalculator: # bmr.py
     def __init__(self, gender, age, weight, height, life_style):
         self.gender = gender
         self.age = age
-        self.weight = weight
-        self.height = height / 100  # cm를 m로 변환
-        self.life_style = float(life_style)
-        self.expectation_bmr = self.calculate_bmr()
+       
 
      
 
