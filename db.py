d1aa599c92d692bc78a6701db389ff4cd7281727
Jane <ssszzy333@gmail.com>
2025-03-11 20:07:49 +0900

    bd 수정

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ class Database:
                 host='localhost',
                 database='test',  # test 데이터베이스 사용
                 user='root',
-                password='tnsrb0313!',  # mariadb 설치 당시의 패스워드, 실제 환경에서는 보안을 위해 환경변수 등을 사용
+                password='12345678',  # mariadb 설치 당시의 패스워드, 실제 환경에서는 보안을 위해 환경변수 등을 사용
                 charset='utf8mb4',
                 cursorclass=pymysql.cursors.DictCursor
             )
@@ -17,8 +17,8 @@ class Database:
         except Error as e:
             print(f"MariaDB 연결 중 오류 발생: {e}")
 
-    def save_bmi_record(self, weight, height, bmi, category):
-        """BMI 기록을 데이터베이스에 저장"""
+    def save_bmr_record(self, gender, age, weight, height, life_style ,expectation_bmr):
+        """BMR 기록을 데이터베이스에 저장"""
         try:
             if self.connection is None:
                 print("데이터베이스 연결이 없습니다.")
@@ -26,20 +26,20 @@ class Database:
                 
             with self.connection.cursor() as cursor:
                 query = """
-                INSERT INTO bmi_records (weight, height, bmi, category)
-                VALUES (%s, %s, %s, %s)
+                INSERT INTO bmr_records(gender, age, weight, height, life_style ,expectation_bmr)
+                VALUES (%s, %s, %s, %s, %s, %s)
                 """
-                cursor.execute(query, (weight, height, bmi, category))
+                cursor.execute(query, (gender, age, weight, height, life_style ,expectation_bmr))
             
             self.connection.commit()
-            print("BMI 기록이 성공적으로 저장되었습니다.")
+            print("BMR 기록이 성공적으로 저장되었습니다.")
             return True
         except Error as e:
             print(f"데이터 저장 중 오류 발생: {e}")
             return False
 
     def get_bmi_records(self, limit=10):
-        """최근 BMI 기록을 가져옵니다"""
+        """최근 BMR 기록을 가져옵니다"""
         try:
             if self.connection is None:
                 print("데이터베이스 연결이 없습니다.")
@@ -47,7 +47,7 @@ class Database:
                 
             with self.connection.cursor() as cursor:
                 query = """
-                SELECT * FROM bmi_records
+                SELECT * FROM bmr_records
                 ORDER BY created_at DESC
                 LIMIT %s
                 """
